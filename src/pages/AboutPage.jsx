b8d0a279a686d0fe06b756645853f183e8f0f363
dipindas-a-h <dipindasofficial@gmail.com>
2024-07-14 11:30:37 +0530

    chjange

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,20 +1,33 @@
-import React from 'react'
-import '../styles/aboutStyle.css'
-import aboutImage from '../../public/about.jpg'
+import React from "react";
+import "../styles/aboutStyle.css";
+import aboutImage from "../../public/about.jpg";
 function About() {
   return (
     <div>
-
-<div className="about-body d-flex align-items-center">
-      <img src={aboutImage} alt="About Us" className='about-image-responsive' />
-      <div className="about-content">
-        <span>About Us</span>
-        <div className="underline-about"></div>
-        <span>fsdgsdfgsdf sdfgsdfg sdfgs dfg sdfg sd fg sdfg sad fg asdfgsdfg sdfg srf g wetrg df gsd fg sdf</span>
+      <div className="about-body d-flex align-items-center">
+        <img
+          src={aboutImage}
+          alt="About Us"
+          className="about-image-responsive"
+        />
+        <div className="about-content">
+          <span>About Us</span>
+          <div className="underline-about"></div>
+          <span className="about-content-text">
+            orem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </span>
+        </div>
       </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default About
+export default About;
